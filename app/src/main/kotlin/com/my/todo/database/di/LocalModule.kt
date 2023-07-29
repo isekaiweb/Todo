177a6c0b077f1de64b50a6b1d5139fcb5446049a
abraham-olin <114895277+abraham-olin@users.noreply.github.com>
2023-07-29 15:23:02 +0700

    :card_file_box: create function to pre-populated data

--- app/src/main/kotlin/com/my/todo/database/di/LocalModule.kt
+++ app/src/main/kotlin/com/my/todo/database/di/LocalModule.kt
@@ -2,29 +2,88 @@ package com.my.todo.database.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.my.todo.R
 import com.my.todo.database.TodoDatabase
 import com.my.todo.database.dao.TodoDao
+import com.my.todo.database.model.TodoEntity
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.concurrent.Executors
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object LocalModule{
+object LocalModule {
 
     @Provides
     @Singleton
     fun providesTodoDatabase(
         @ApplicationContext context: Context,
+        todoDao: TodoDao
     ): TodoDatabase = Room.databaseBuilder(
         context = context,
         klass = TodoDatabase::class.java,
         name = "todo_database"
-    ).build()
+    ).addCallback(object : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            Executors.newSingleThreadExecutor().execute {
+                fillWithStartingData(context, todoDao)
+            }
+        }
+    }).build()
 
+    private fun fillWithStartingData(context: Context, dao: TodoDao) {
+        val jsonArray = loadJsonArray(context)
+        try {
+            if (jsonArray != null) {
+                for (i in 0 until jsonArray.length()) {
+                    val item = jsonArray.getJSONObject(i)
+                    dao.insertAll(
+                        TodoEntity(
+                            id = item.getInt("id"),
+                            title = item.getString("title"),
+                            description = item.getString("description"),
+                            dueDate = item.getString("dueDate")
+                        )
+                    )
+                }
+            }
+        } catch (exception: JSONException) {
+            exception.printStackTrace()
+        }
+    }
+
+    private fun loadJsonArray(context: Context): JSONArray? {
+        val builder = StringBuilder()
+        val `in` =
+            context.resources.openRawResource(R.raw.todos)
+        val reader = BufferedReader(InputStreamReader(`in`))
+        var line: String?
+        try {
+            while (reader.readLine().also { line = it } != null) {
+                builder.append(line)
+            }
+            val json = JSONObject(builder.toString())
+            return json.getJSONArray("todos")
+        } catch (exception: IOException) {
+            exception.printStackTrace()
+        } catch (exception: JSONException) {
+            exception.printStackTrace()
+        }
+        return null
+    }
 
     @Provides
     @Singleton
