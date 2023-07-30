0aa924b70796cb52f8c7d97c4ab1384f443323eb
abraham-olin <114895277+abraham-olin@users.noreply.github.com>
2023-07-30 10:00:00 +0700

    :lipstick: set testTagsAsResourceId to true

--- app/src/main/kotlin/com/my/todo/MainActivity.kt
+++ app/src/main/kotlin/com/my/todo/MainActivity.kt
@@ -6,20 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import com.my.todo.feature.TodoRoute
 import com.my.todo.ui.theme.TodoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().semantics { testTagsAsResourceId = true },
                     color = MaterialTheme.colorScheme.background
                 ) {
                     TodoRoute()
